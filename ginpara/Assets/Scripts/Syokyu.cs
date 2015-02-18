5cc1931b03fa4df042bce01e9196d77f69981ffc
Toni Takekawa <tonitakekawa@gmail.com>
2015-02-18 12:40:33 +0900

    釘追加

---
+++ ginpara/Assets/Scripts/Syokyu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syokyu : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision:");
+        if (collision.gameObject.tag == "Ball")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
