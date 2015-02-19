1076a1b86d191f69345fc60f5693a5f95ba87bf9
Toni Takekawa <tonitakekawa@gmail.com>
2015-02-20 02:51:51 +0900

    スクロール！

---
+++ ginpara/Assets/Scripts/scroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<UIScrollView>().Scroll(5f * Time.deltaTime);
+	}
+}
