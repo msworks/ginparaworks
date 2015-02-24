b98440d8b189edceed2863346bd9556d8f212c3b
Toni Takekawa <tonitakekawa@gmail.com>
2015-02-24 22:47:57 +0900

    玉が入ったら抽選開始

---
+++ ginpara/Assets/Scripts/Syokyu_Nyusyo0.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// チャッカー
+public class Syokyu_Nyusyo0 : MonoBehaviour {
+
+    public GameObject Horyu;
+
+    const string msg = "チャッカー通過";
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // TODO 玉を増やす処理
+        if (collision.gameObject.tag == "Ball")
+        {
+            Destroy(collision.gameObject);
+        }
+
+        // 保留オブジェクトにチャッカー通過メッセージを送る
+        Horyu.GetComponent<PlayMakerFSM>().SendEvent(msg);
+
+    }
+
+}
