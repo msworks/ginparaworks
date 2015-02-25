0c8935e88c8a23d0a9171c68cde0bad08faf019f
Toni Takekawa <tonitakekawa@gmail.com>
2015-02-25 21:35:53 +0900

    風車対応

---
+++ ginpara/Assets/Scripts/SpinFusya.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinFusya : MonoBehaviour {
+
+    public GameObject Fusya;
+    public string AnimationName;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Fusya.GetComponent<Animation>().Play(AnimationName);
+    }
+
+}
