ba427ed298261cbc666131c0647645759957b989
Toni Takekawa <tonitakekawa@gmail.com>
2015-02-26 19:34:11 +0900

    回転体対応

---
+++ ginpara/Assets/Scripts/TouchKaitentai.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 回転体に玉が乗ったときの挙動
+// ボールの状態を変化させる。自律的にやる。
+// 
+public class TouchKaitentai : MonoBehaviour {
+
+    private string msg = "TouchKaitentai";
+
+    public GameObject TurnBallPrefab;
+    public GameObject BodyPath;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+
+        var ball = NGUITools.AddChild(BodyPath, TurnBallPrefab);
+        //ball.transform.position = ShootPosition.transform.position;
+        //ball.rigidbody2D.velocity = ShootPower * rndp;
+    }
+
+}
