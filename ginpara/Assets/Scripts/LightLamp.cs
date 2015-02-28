4359fd43cd4e6e92f8b5e1ca01259752f6d2e499
Toni Takekawa <tonitakekawa@gmail.com>
2015-02-28 22:56:24 +0900

    ランプをつけた

---
+++ ginpara/Assets/Scripts/LightLamp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightLamp : MonoBehaviour {
+
+    public GameObject right;
+    public GameObject left;
+
+    private bool LightFlg;
+    private int Counter = 0;
+
+	// Use this for initialization
+	void Start () {
+        LightFlg = true;
+        Counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!LightFlg) return;
+
+        Counter++;
+
+        var r = (float)Counter * 3.14f / 60f * 5f;
+        var v = Mathf.Sin(r) * Mathf.Sin(r);
+        var v2 = 1f - v;
+
+        right.GetComponent<UISprite>().alpha = v;
+        left.GetComponent<UISprite>().alpha = v2;
+
+	}
+}
