b1d04d1f92090c33f47157ef69985522fca4ae13
Toni Takekawa <tonitakekawa@gmail.com>
2015-03-27 01:39:13 +0900

    DISPLAYボタン対応

---
+++ ginpara/Assets/Scripts/WebButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ウェブボタン
+/// </summary>
+public class WebButton : MonoBehaviour {
+
+    /// <summary>
+    /// タップ時処理
+    /// </summary>
+    public void OnClick()
+    {
+        var url = "http://www.yahoo.co.jp/";
+        var webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init((msg) =>
+        {
+            if (msg == "clicked")
+            {
+                webViewObject.SetVisibility(false);
+            }
+        });
+        webViewObject.LoadURL(url);
+        webViewObject.SetMargins(50, 100, 50, 50);
+        webViewObject.SetVisibility(true);
+        webViewObject.EvaluateJS(
+            "window.addEventListener('load', function() {" +
+            "   window.addEventListener('click', function() {" +
+            "       Unity.call('clicked');" +
+            "   }, false);" +
+            "}, false);");
+    }
+	
+}
